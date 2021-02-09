e30053dcf1c81c66271e33a445a7ab59effcde07
Carlos Andres <darkklitos@gmail.com>
2021-02-08 19:40:06 -0500

    add categoryList

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -10,28 +10,41 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 public class BlogController {
-
-    private PostRepository postRepository;
-
-    public BlogController(PostRepository postRepository) {
-        this.postRepository = postRepository;
-    }
-
-    @RequestMapping("/")
-    public String listPosts(ModelMap modelMap) {
-        List<Post> posts = postRepository.findAll();
-        modelMap.put("posts", posts);
-        return "home";
-    }
-
-    @RequestMapping("/post/{id}")
-    public String postDetails(@PathVariable Long id, ModelMap modelMap) {
-        Post post = postRepository.findById(id).orElse(null);
-        modelMap.put("post", post);
-        return "post-details";
-    }
+	
+	private PostRepository postRepository;
+	private CategoryRepository categoryRepository;
+	
+	public BlogController ( PostRepository postRepository, CategoryRepository categoryRepository ) {
+		
+		this.postRepository = postRepository;
+		this.categoryRepository = categoryRepository;
+	}
+	
+	@RequestMapping ( "/" )
+	public String listPosts ( ModelMap modelMap ) {
+		List < Post > posts = postRepository.findAll ( );
+		List < Category > categories = categoryRepository.findAll ( );
+		modelMap.put ( "posts", posts );
+		modelMap.put ( "categories", categories );
+		return "home";
+	}
+	
+	@RequestMapping ( "/post/{id}" )
+	public String postDetails ( @PathVariable Long id, ModelMap modelMap ) {
+		Post post = postRepository.findById ( id ).orElse ( null );
+		modelMap.put ( "post", post );
+		return "post-details";
+	}
+	
+	@RequestMapping ( "/category/{id}" )
+	public String categoryList ( @PathVariable Long id, ModelMap modelMap ) {
+		Category category = categoryRepository.findById ( id ).orElse ( null );
+		List < Post > posts = postRepository.findAll ( );
+		modelMap.put ( "category", category );
+		modelMap.put ( "posts", posts );
+		return "post-details";
+	}
 }
